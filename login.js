3458cbd27553f6474e16ed2ffc1ac81c8394b5c6
lewisjenner <lewis.jenner@hartmann.info>
2018-01-16 16:06:05 +0000

    First Commit

---
+++ login.js
@@ -0,0 +1,9 @@
+function inputCheck() {
+
+  var userName = document.getElementById('login-name');
+  var passWord = document.getElementById('login-pass');
+
+  if(userName.value == null || passWord.trim == null) {
+    alert('1213');
+  }
+}
